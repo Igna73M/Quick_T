7281a723f563f80ec429e84c7feae41fcf2b986b
MUGABE Nshuti Ignace <ignacemugabe@gmail.com>
2025-10-03 09:32:48 +0200

    Update: project folder structure

--- client/src/components/layout/layoutdummy.tsx
+++ client/src/components/layout/layoutdummy.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Register() {
+function layoutdummy() {
   return <div></div>;
 }
 
-export default Register;
+export default layoutdummy;
--- client/src/utils/dumm.tsx
+++ client/src/utils/dumm.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-function Register() {
+function dumm() {
   return <div></div>;
 }
 
-export default Register;
+export default dumm;
